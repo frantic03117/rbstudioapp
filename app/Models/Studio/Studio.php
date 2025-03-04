49c38960183eab6acf8e593cdc773e6c3200ca48
frantic03117 <shahrukh@franticpro.com>
2025-03-04 15:36:09 +0530

    updated

--- app/Models/Studio/Studio.php
+++ app/Models/Studio/Studio.php
@@ -20,9 +20,9 @@ class Studio extends Model
 
     public function vendor(): HasOne
     {
-        return $this->hasOne(Vendor::class, "id", "vendor_id")
-        ->join("users","users.vendor_id","=","vendors.id")->where('role', 'Admin')
-        ->select('vendors.*', 'users.email', 'users.mobile');
+        return $this->hasOne(Vendor::class, "vendors.id", "vendor_id")
+            ->join("users", "users.vendor_id", "=", "vendors.id")->where('role', 'Admin')
+            ->select('vendors.*', 'users.email', 'users.mobile');
     }
     public function country(): HasOne
     {
@@ -41,13 +41,16 @@ class Studio extends Model
         return $this->hasMany(StudioImage::class, "studio_id", "id");
     }
 
-    public function products() {
-        return $this->belongsToMany(Rent::class,Charge::class,'studio_id','item_id')->where('type', '=', 'Item')->as('resources')->withPivot(['charge', 'created_at']);
+    public function products()
+    {
+        return $this->belongsToMany(Rent::class, Charge::class, 'studio_id', 'item_id')->where('type', '=', 'Item')->as('resources')->withPivot(['charge', 'created_at']);
     }
-    public function charges(){
-        return $this->belongsToMany(Service::class,ServiceStudio::class, 'studio_id','service_id')->as('charge')->withPivot(['charge', 'is_permissable', 'id']);
+    public function charges()
+    {
+        return $this->belongsToMany(Service::class, ServiceStudio::class, 'studio_id', 'service_id')->as('charge')->withPivot(['charge', 'is_permissable', 'id']);
     }
-    public function services(){
-        return $this->belongsToMany(Service::class,ServiceStudio::class, 'studio_id','service_id')->as('charge')->withPivot(['charge', 'is_permissable', 'id']);
+    public function services()
+    {
+        return $this->belongsToMany(Service::class, ServiceStudio::class, 'studio_id', 'service_id')->as('charge')->withPivot(['charge', 'is_permissable', 'id']);
     }
 }
