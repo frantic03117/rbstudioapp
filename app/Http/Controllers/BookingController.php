67a71af73fe236117b887cbd377acbf0ee604559
frantic03117 <shahrukh@franticpro.com>
2025-04-04 16:48:32 +0530

    updated

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -128,14 +128,13 @@ class BookingController extends Controller
 
 
             if ($approved_at == "pending") {
-                $items->where('approved_at', null)->where('booking_status', "0");
-            } else {
-                if ($booking_status) {
-                    $items->where('booking_status', $booking_status);
-                }
-                if ($booking_status == "0") {
-                    $items->where('booking_status', "0");
-                }
+                $items->where('approved_at', null);
+            }
+            if ($booking_status) {
+                $items->where('booking_status', $booking_status);
+            }
+            if ($booking_status == "0") {
+                $items->where('booking_status', "0");
             }
 
             if ($approved_at == "approved") {
