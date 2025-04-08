58efe421b84eaf4e99cce019652e04e5c4d1cdd9
frantic03117 <shahrukh@franticpro.com>
2025-04-08 19:01:57 +0530

    updated

---
+++ database/migrations/2025_04_08_185817_create_extra_booking_amounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extra_booking_amounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('booking_id');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extra_booking_amounts');
+    }
+};
