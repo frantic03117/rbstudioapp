9c34a0e081310feb59ef9f755ff5ca8b7e1394f8
frantic03117 <shahrukh@franticpro.com>
2025-04-19 17:13:32 +0530

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -91,7 +91,7 @@ class AdminController extends Controller
         if (Auth::user()->role != "Super") {
             $aproval->where('vendor_id', Auth::user()->vendor_id);
         }
-        $aproval->whereYear('booking_start_date', date('Y'))->where('booking_status', '=', '1');
+        $aproval->whereYear('booking_start_date', date('Y'));
         $approval = $aproval->where('approved_at', null)->count();
         $vends = Vendor::orderBy('name');
         if (Auth::user()->role != "Super") {
