c9046e8dbf1b37777d5fd836c4205e788930b41f
frantic03117 <shahrukh@franticpro.com>
2025-04-28 16:35:04 +0530

    jsonend

--- resources/views/admin/reports/details.blade.php
+++ resources/views/admin/reports/details.blade.php
@@ -1,24 +1,25 @@
-<div class="modal fade" id="staticBackdrop{{$item->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">
-            {{$item->id}}
-        </h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-            @foreach(json_decode($item->ret_resp)  as $key => $val)
-                <div class="row">
-                    <div class="col-6 border p-1  text-capitalize">
-                      {!! implode(' ', explode('_', $key)) !!}
+<div class="modal fade" id="staticBackdrop{{ $item->id }}" data-bs-backdrop="static" data-bs-keyboard="false"
+    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                    {{ $item->id }}
+                </h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                @foreach (json_decode($item->ret_resp) as $key => $val)
+                    <div class="row">
+                        <div class="col-6 border p-1  text-capitalize">
+                            {!! implode(' ', explode('_', $key)) !!}
+                        </div>
+                        <div class="col-6 border p-1">
+                            {{ json_encode($val) }}
+                        </div>
                     </div>
-                    <div class="col-6 border p-1">
-                        {{$val}}
-                    </div>
-                </div>
-            @endforeach
-      </div>
+                @endforeach
+            </div>
+        </div>
     </div>
-  </div>
 </div>
