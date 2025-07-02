c80f86c175c75d40e031655fd67cde5b7208aa83
frantic03117 <shahrukh@franticpro.com>
2025-07-02 13:22:06 +0530

    booking_start_date

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -202,7 +202,7 @@ class AdminController extends Controller
         if ($booking_date_to) {
             $books->whereDate('booking_start_date', '<=', $booking_date_to);
         }
-        $items = $books->with('studio:id,name,color')->with('vendor')->orderBy('booking_date_from', 'ASC')->get();
+        $items = $books->with('studio:id,name,color')->with('vendor')->orderBy('booking_start_date', 'ASC')->get();
         //   return response()->json($items);
         //   die;
 
