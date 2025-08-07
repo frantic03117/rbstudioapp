1f447e028872763bc85afd8475060a77e7c9b474
frantic03117 <shahrukh@franticpro.com>
2025-08-07 15:06:40 +0530

    updated

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,6 +30,15 @@ class AuthController extends Controller
         } else {
             $mobile = $request->mobile;
             $check = User::where(['mobile' =>  $mobile])->first();
+            if ($check->deleted_at) {
+                $data = [
+                    'errors' => $validator->errors(),
+                    'data' => [],
+                    'success' => 0,
+                    'message' =>  'Account deleted. Please contact us to activate your account'
+                ];
+                return  response()->json($data, 200);
+            }
             $otp  = $mobile  == "8888888888" ? '8888' : mt_rand(1111, 9999);
             if ($check) {
                 if ($check->role != "User") {
