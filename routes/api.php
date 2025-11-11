b90dbf5fd27b391329ed7daae2031daa25766d63
frantic03117 <shahrukh@franticpro.com>
2025-11-11 13:25:31 +0530

    updated

--- routes/api.php
+++ routes/api.php
@@ -111,6 +111,7 @@ Route::middleware(['auth:sanctum', 'checkrole:Super,Admin,Employee'])->prefix('a
     Route::post('booking_item-add', [BookingController::class, 'booking_item_add']);
     Route::post('extra-amount/store', [ExtraBookingAmountController::class, 'store']);
     Route::post('update-gst-details', [BookingController::class, 'update_gst_details']);
+    Route::post('update-tds/{id}', [BookingController::class, 'tds_control']);
 
     Route::post('update_rental_item', [BookingController::class, 'update_rental_item_in_booking']);
     Route::post('remove_rental_item_from_booking', [BookingController::class, 'remove_rental_item_from_booking']);
